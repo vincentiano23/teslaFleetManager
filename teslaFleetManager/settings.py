9b1661b51d216b19cece64931a6839b82c87d866
vincentiano23 <vincentmuthiani15@gmail.com>
2024-11-04 13:13:51 +0300

    added base.html as main layout template with bootstrap styling for improved UI consistency -created home.html and set it to extend base.html -updated other templates to extend base.html for unified structure and easier maintenance -implemented a sticky footer using Flexbox in base.html

--- teslaFleetManager/settings.py
+++ teslaFleetManager/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'teslaFleetManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'TeslaFleetManager' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- teslaFleetManager/urls.py
+++ teslaFleetManager/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('vehicles/', include('vehicle_data.urls')),
     path('battery/', include('battery.urls')),
     path('telemetry/', include('telemetry.urls')),
